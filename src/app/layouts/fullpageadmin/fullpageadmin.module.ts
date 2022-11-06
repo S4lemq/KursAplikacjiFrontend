cc95b2a4061ac157a0f6a0e9d81ded1557939d3f
Piotr <peter.lubowicz@gmail.com>
2022-11-06 13:37:49 +0100

    stylowanie panelu admina

--- src/app/layouts/fullpageadmin/fullpageadmin.module.ts
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -4,6 +4,7 @@ import {FullpageadminComponent} from "./fullpageadmin.component";
 import {AdminComponent} from "../../modules/admin/admin.component";
 import {RouterModule} from "@angular/router";
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {MaterialModule} from "../../shared/material.module";
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import {FlexLayoutModule} from "@angular/flex-layout";
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class FullpageadminModule { }
